f2f0537ec321b3cd998f2003cde53259b161008b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-10-29 06:56:44 -0300

    Create styles.ts

---
+++ src/components/SearchPost/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const SeachPostContainer = styled.form`
+  width: 95%;
+
+  > div{
+    display: flex;
+    justify-content: space-between;
+
+    h3{
+      color: #C4D4E3;
+    }
+    
+    span{
+      color: #C4D4E3;
+    }
+  }
+  
+  input{
+    border-radius: 6px;
+    padding: 0.75rem 1rem;
+    width: 100%;
+    margin-top: 1rem;
+    background: #040F1A;
+    border: 1px solid #1C2F41;
+    color: #7B96B2;
+  }
+`
