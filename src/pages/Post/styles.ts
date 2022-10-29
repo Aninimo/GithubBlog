0a4333a8dff34cb5dc3d17d0d8d51dd92f64e7bc
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-10-29 06:50:52 -0300

    Create styles.ts

---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.div`
+  margin-top: -4.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0 1rem;
+`
+
+export const PostCard = styled.div`
+  max-width: 864px;
+  width: 100%;
+  height: auto;
+  padding:  2rem;
+  background: #0B1B2B;
+  box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+`
