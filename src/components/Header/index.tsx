a6b5c322c75766bd89d269bdbbdb3f259f07517f
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-10-29 06:52:22 -0300

    Create index.tsx

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from './styles'
+
+export function Header(){
+  return(
+    <HeaderContainer>   
+      <img src='https://raw.githubusercontent.com/pedr0d1as/Github-Blog/646d649d1cd66399e6ea340ca27c127cc122247f/src/assets/tech-bg.svg'/>
+    </HeaderContainer>
+  )
+}
